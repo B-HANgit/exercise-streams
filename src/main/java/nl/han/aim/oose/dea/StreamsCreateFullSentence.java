35c8ae1f81ffaff561f60dd078eb59d24c83383a
B-HANgit <BAH.vanderVen@student.han.nl>
2023-02-09 20:24:31 +0100

    opdracht 2+3

--- src/main/java/nl/han/aim/oose/dea/StreamsCreateFullSentence.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsCreateFullSentence.java
@@ -2,11 +2,12 @@ package nl.han.aim.oose.dea;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsCreateFullSentence {
     String createAFullSentenceFromTheList(List<String> input) {
         // TODO: implement
-
-        return "";
+        return input.stream()
+                .collect(Collectors.joining(" "));
     }
 }
--- src/main/java/nl/han/aim/oose/dea/StreamsFindShortestString.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsFindShortestString.java
@@ -8,10 +8,15 @@ import java.util.stream.Stream;
 public class StreamsFindShortestString {
     String findShortestString(List<String> input) {
         // TODO: implement
-        Stream<String> stream = input.stream();
-//        var answer = stream.min(Comparator.comparingInt(String::length));
+//        Stream<String> stream = input.stream();
+////        var answer = stream.min(Comparator.comparingInt(String::length));
+//
+//        Optional<String> answer = stream.min((s, p) -> {return s.length() - p.length();});
+//        answer.orElse(null);
+//
+//        return answer.get();
 
-        Optional<String> answer = stream.min((s, p) -> {return s.length() - p.length();});
-        return answer.get();
+
+        return input.stream().min((s,p)-> {return s.length() - p.length();}).orElse(null);
     }
 }
