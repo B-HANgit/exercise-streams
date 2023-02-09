e99e882ba74ae465e8010a25f9c7d0aaf89c784b
B-HANgit <BAH.vanderVen@student.han.nl>
2023-02-09 20:06:13 +0100

    v1

--- src/main/java/nl/han/aim/oose/dea/StreamsFilterList.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsFilterList.java
@@ -2,18 +2,33 @@ package nl.han.aim.oose.dea;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsFilterList {
 
     List<String> filterStringsShorterThanThreeCharacters(List<String> input) {
         // TODO: implement
-
-        return input;
+        Stream<String> stream = input.stream();
+        var filteredlist = stream.filter(s -> s.length() < 3);
+        return filteredlist.toList();
     }
 
     List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
         // TODO: implement
+        Stream<String> stream = input.stream();
+        var filteredlist = stream.filter(s -> check(s));
+        return filteredlist.toList();
+    }
 
-        return input;
+    private boolean check(String item){
+        boolean bool;
+        try{
+            Integer.parseInt(item);
+            bool = true;
+        }
+        catch(Exception e) {
+            bool = false;
+        }
+        return bool;
     }
 }
--- src/main/java/nl/han/aim/oose/dea/StreamsFindShortestString.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsFindShortestString.java
@@ -2,11 +2,16 @@ package nl.han.aim.oose.dea;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class StreamsFindShortestString {
     String findShortestString(List<String> input) {
         // TODO: implement
+        Stream<String> stream = input.stream();
+//        var answer = stream.min(Comparator.comparingInt(String::length));
 
-        return "";
+        Optional<String> answer = stream.min((s, p) -> {return s.length() - p.length();});
+        return answer.get();
     }
 }
