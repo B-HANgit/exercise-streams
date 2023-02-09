8a332c697c19496f8699d424d2471bc1535427cf
B-HANgit <BAH.vanderVen@student.han.nl>
2023-02-09 20:44:47 +0100

    idk2

--- src/main/java/nl/han/aim/oose/dea/StreamsCalculateTotalCost.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsCalculateTotalCost.java
@@ -4,12 +4,21 @@ import nl.han.aim.oose.dea.helpers.Product;
 import nl.han.aim.oose.dea.helpers.ProductCategory;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamsCalculateTotalCost {
     int calculateTotalCostOfAllProducts(List<Product> products) {
         // TODO: implement
 
+//        return products.stream();
+//                .???;
         return 0;
+
+//          ANSWER:
+//        products.stream()
+//                .mapToInt(Product::getPrice)
+//                .sum();
     }
 
     int calculateTotalCostOfAllGadgets(List<Product> products) {
